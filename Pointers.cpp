63e15db7de32db79d81d9782cf7fa465e9e67ab4
Pranav Thombare <pranavthombare97@gmail.com>
2019-04-28 11:29:47 +0100

    cpp: Code explanation added.

--- Pointers.cpp
+++ Pointers.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-//
+
 int x=5;
 int *ptr = &x;
 
-cout<<ptr<<endl; // this will give you the address of
-cout<<*ptr<<endl;
+cout<<ptr<<endl; // this will give you the address of x
+cout<<*ptr<<endl; // this will give you the value of x
 return 0;
 
 }
